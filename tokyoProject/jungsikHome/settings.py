55aa3f8ec35fd03f29cb98b4e95035b5cc541769
Jungsik <sk7556@naver.com>
2023-12-12 15:14:44 +0900

    cors_allow 변경

--- tokyoProject/jungsikHome/settings.py
+++ tokyoProject/jungsikHome/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+
+env = os.environ.get
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pc!!9yfkmdm#*x9q7t_ctr7)s(4fehbi+!2fugy_aq8c$puq*='
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
