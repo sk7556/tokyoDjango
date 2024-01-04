8d95e88b089b02e470153ccee76bd085b4b10f08
Jungsik <sk7556@naver.com>
2024-01-04 16:19:35 +0900

    static image, url error fix

--- tokyoProject/jungsikHome/urls.py
+++ tokyoProject/jungsikHome/urls.py
@@ -4,11 +4,10 @@ from .views import IndexAPIView, LoginPageAPIView, index, login_page
 urlpatterns = [
     path('api/index/', IndexAPIView.as_view(), name='api_index'),
     path('api/login/', LoginPageAPIView.as_view(), name='api_login_page'),
-    path('api/v1/', index, name='index'), # api/v1/ 에 대한 정의가 필요하다는 GPT의 억지에 타보
     path('', index, name='index'), 
     path('login/', login_page, name='login_page'),
     # 외부 앱 연결 
-    path('todo/', include('toDo.urls')),
+    path('toDo/', include('toDo.urls')),
     path('resume/', include('resume.urls')),
     path('diary/', include('diary.urls')),
 ]
