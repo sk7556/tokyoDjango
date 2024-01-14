49274fd3b7684997308b071576d4e0a5c0f346b4
Jungsik <sk7556@naver.com>
2024-01-14 19:05:47 +0900

    init resume

--- tokyoProject/resume/models.py
+++ tokyoProject/resume/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Resume(models.Model):
+    name        = models.CharField(max_length=100)
+    comment     = models.TextField()
+    career      = models.TextField()
+    role        = models.TextField()
+    salary      = models.TextField()
+    intro       = models.TextField()
+    information = models.TextField()
+    certificate = models.TextField()
+    
+    def __str__(self):
+        return self.title
