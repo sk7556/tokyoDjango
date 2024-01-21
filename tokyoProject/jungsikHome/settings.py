a140e28e24acb19a0c74345b4466f945b1df4a98
Jungsik <sk7556@naver.com>
2024-01-21 15:16:09 +0900

    Revert "cors_allow 변경"
    This reverts commit 55aa3f8ec35fd03f29cb98b4e95035b5cc541769.

--- tokyoProject/jungsikHome/settings.py
+++ tokyoProject/jungsikHome/settings.py
@@ -1,7 +1,4 @@
 from pathlib import Path
-import os
-
-env = os.environ.get
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,9 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-pc!!9yfkmdm#*x9q7t_ctr7)s(4fehbi+!2fugy_aq8c$puq*='
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
