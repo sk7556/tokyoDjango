9b0df23db00aa2446be76483f73285f9be7e3106
Jungsik <sk7556@naver.com>
2024-01-24 12:05:03 +0900

    loginpage test

--- tokyoProject/jungsikHome/settings.py
+++ tokyoProject/jungsikHome/settings.py
@@ -118,11 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = '/path/to/static/folder'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/path/to/media/folder'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tokyoProject/jungsikHome/urls.py
+++ tokyoProject/jungsikHome/urls.py
@@ -4,7 +4,8 @@ from .views import IndexAPIView, LoginPageAPIView, index, login_page
 urlpatterns = [
     path('api/index/', IndexAPIView.as_view(), name='api_index'),
     path('api/login/', LoginPageAPIView.as_view(), name='api_login_page'),
-    path('index/', index, name='index'),
+    path('api/v1/', index, name='index'),
+    path('', index, name='index'),
     path('login/', login_page, name='login_page'),
     # 외부 앱 연결 
     path('todo/', include('toDo.urls')),
