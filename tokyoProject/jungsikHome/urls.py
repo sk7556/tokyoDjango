83ae9ac23713e637f196831d03b976a9da83565b
Jungsik <sk7556@naver.com>
2024-02-04 18:43:34 +0900

    jwt creating

--- tokyoProject/jungsikHome/urls.py
+++ tokyoProject/jungsikHome/urls.py
@@ -2,14 +2,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from django.urls import path, include
-from .views import IndexAPIView, LoginPageAPIView, index, login_page
+from .views import IndexAPIView, LoginPageAPIView, index, login_view, logout_view
 
 
 urlpatterns = [
     path('api/index/', IndexAPIView.as_view(), name='api_index'),
     path('api/login/', LoginPageAPIView.as_view(), name='api_login_page'),
     path('', index, name='index'), 
-    path('login/', login_page, name='login_page'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     # 외부 앱 연결 
     path('todo/', include('toDo.urls')),
     path('resume/', include('resume.urls')),
--- tokyoProject/jungsikHome/views.py
+++ tokyoProject/jungsikHome/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 class IndexAPIView(APIView):
     def get(self, request):
@@ -14,5 +17,34 @@ class LoginPageAPIView(APIView):
 def index(request):
     return render(request, "jungsikHome/index.html")
 
-def login_page(request):
-    return render(request, "jungsikHome/login.html")
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # 로그인 성공 시 리다이렉트 또는 다른 동작 수행
+            return redirect('afterLogin')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    # 로그아웃 후 리다이렉트 또는 다른 동작 수행
+    return redirect('afterLogout')
+
+
+@login_required
+def afterLogin(request):
+    # 로그인한 사용자만 접근 가능한 뷰
+    pass
+
+def afterLogout(request):
+    pass
+
+@permission_required('some_app.can_view_content')
+def restricted_view(request):
+    # 특정 권한이 있는 사용자만 접근 가능한 뷰
+    pass
