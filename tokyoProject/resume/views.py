512237f8b793b43bdf92d9252dd57b6fff07023c
Jungsik <sk7556@naver.com>
2024-02-16 16:48:33 +0900

    login option check

--- tokyoProject/resume/views.py
+++ tokyoProject/resume/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .models import Resume
 from .serializers import ResumeSerializer
 
@@ -10,14 +12,16 @@ class ResumeAPIView(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
     def get_object(self):
-        # 항상 첫 번째 Resume 객체만을 반환합니다.
         return self.queryset.first()
     
 def resume_page(request):
     resume = Resume.objects.first()
     return render(request, "resume/resume.html", {'resume': resume})
 
+@login_required(login_url='/login/')
 def update_resume(request):
     resume = Resume.objects.first()
-    return render(request, 'resume/update_resume.html', {'resume': resume})
-
+    if request.user.is_authenticated:
+        return render(request, 'resume/update_resume.html', {'resume': resume})
+    else:
+        return HttpResponse("로그인 된 사용자만 사용 가능합니다.", status=403)
