96f1cd4a42c7fd62a917a84ad005dcaaaa4d9477
Jungsik <sk7556@naver.com>
2024-04-25 10:21:11 +0900

    자작 middleware 컷

--- tokyoProject/jungsikHome/settings.py
+++ tokyoProject/jungsikHome/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'diary.JWTmiddleware.JWTAuthenticationMiddleware',
 ]
 
 REST_FRAMEWORK = {
